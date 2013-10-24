a700d5d0e5dfe3c7b2c15ad3a17d11457c30b803
jesswu <lw353@cornell.edu>
2013-10-23 23:49:37 -0400

    rewrote to fit satisfiability

--- GA.m
+++ GA.m
@@ -1,16 +1,16 @@
-function [solution, sbest] = GA(Xinitial, popsize, maxGen, pCrossover, pMutation)
+function [solution, sbest] = GA(Xinitial, popsize, maxGen, pCrossover, pMutation, constraints)
 
 solution = zeros(maxGen,3);
-sbest = zeros(maxGen,2);
+sbest = zeros(maxGen,200);
 currentPop = Xinitial; 
 fit = zeros(1,popsize);
 for i = 1:popsize
-fit(i) = fitness(currentPop(i,:));
+fit(i) = costSAT(currentPop(i,:),constraints);
 end
 
 for i = 1:maxGen
-
-children = zeros(20,14);
+solution(i,:) = [i, mean(fit), max(fit)];
+children = zeros(20,200);
 for j = 1:10
 [parent1, parent2] = selection_R(currentPop,fit);
 [children(j,:),children(j+10,:)] = crossover(parent1,parent2,pCrossover);
@@ -22,7 +22,7 @@ end
 
 childFit = zeros(1,20);
 for j = 1:20
-childFit(j) = fitness(children(j,:));
+childFit(j) = costSAT(children(j,:),constraints);
 end
 
 indexWeak = find(childFit==min(childFit),1);
@@ -31,17 +31,58 @@ children(indexWeak,:) = currentPop(indexStrong,:);
 currentPop = children;
 
 for j = 1:popsize
-fit(j) = fitness(currentPop(j,:));
+fit(j) = costSAT(currentPop(j,:),constraints);
 end
 
-sbest(i,1) = bin2dec(strcat(num2str(currentPop(find(fit==max(fit),1),1:7))));
-sbest(i,2) = bin2dec(strcat(num2str(currentPop(find(fit==max(fit),1),8:14))));
-solution(i,:) = [i, mean(fit), max(fit)];
+
+sbest(i,1:200) = currentPop(find(fit==max(fit),1),:);
+
 end
 end
 
-pCrossover = .9;
-pMutation = .05;
-popsize = 20;
-maxGen = 50;
-Xinitial = round(rand(20,14));	
+
+function [child1, child2] = crossover(parent1,parent2,pCrossover)
+if (rand()<=pCrossover)
+cross = ceil(rand()*200);
+child1 = cat(2,parent1(1:cross),parent2(cross+1:200));
+child2 = cat(2,parent2(1:cross),parent1(cross+1:200));
+else
+child1 = parent1;
+child2 = parent2;
+end
+end
+
+function [child] = mutation(s, pMutation)
+child = s;
+for i = 1:length(s)
+if (rand()<=pMutation)
+child(i) = 1-child(i);
+end
+end
+end
+
+
+function [parent1,parent2] = selection_R(population,fit)
+prob = cumsum(fit/sum(fit));
+select1 = rand();
+index1 = 0;
+for i = 1:length(population)
+if (index1==0 && select1<prob(i))
+index1 = i;
+end
+end
+parent1 = population(index1,:);
+
+index2 = index1;
+while index2==index1
+select2 = rand();
+tmp = 0;
+for i = 1:length(population)
+if (tmp==0 && select2<prob(i))
+tmp = i;
+end
+end
+index2 = tmp;
+end
+parent2 = population(index2,:);	
+end
